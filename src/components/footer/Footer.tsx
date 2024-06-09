27fac5de8d340437283cce7c1ca637a1bd24af46
ker1200 <60584013+ker1200@users.noreply.github.com>
2024-06-08 20:04:17 -0700

    fix about css and add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,19 @@
 import "./footer.css"
+import { SocialIcon } from 'react-social-icons'
 
 const Footer = () => {
     return (
-        <h1>footer</h1>
+        <div className="footer-container">
+            <div className="icon">
+                <SocialIcon url="https://github.com/kerryz12" />
+            </div>
+            <div className="icon">
+                <SocialIcon url="https://linkedin.com/kerry-zhang-ee" />
+            </div>
+            <div className="icon">
+                <SocialIcon url="mailto:kerryzhang12@gmail.com" />
+            </div>
+        </div>
     )
 }
 
